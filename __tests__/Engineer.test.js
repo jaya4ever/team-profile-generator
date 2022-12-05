3822da551890aa38122b35e15034f2f06750ef57
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-05 10:45:06 -0600

    added the engineer test and it passed

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
+
+test("Can set GitHUb account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "workingemployee@gmail.com", testValue);
+    expect(e.github).toBe(testValue);
+  });
+  
+  test("getRole() should return Engineer" , () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Foo", 1, "workingemployee@gmail.com", "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+  });
+  
+  test("Can get GitHub username through getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "workingemployee@gmail.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+  });
