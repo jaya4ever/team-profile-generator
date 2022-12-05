b7e4ba60879353a3f26df699d050048594d3de80
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-05 10:33:16 -0600

    added the test for employee and passes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,24 @@
 const Employee = require("../lib/Employee");
+
+test('creating a employee object', () => {
+    const employee = new Employee('Suresh');
+});
+
+test('setting id with constructor', () => {
+    const testValue = 100;
+    const e = new Employee('Mohit', testValue);
+    expect(e.id).toBe(testValue);
+});
+
+test('setting email with constructor', () => {
+    const testValue = 'workingemployee@gmail.com';
+    const e = new Employee('Mohit', 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+// Test if the getRole() value is Employee
+test('getRole() return Employee', () => {
+    const testValue = 'Employee';
+    const e = new Employee('Suresh', 1, 'workingemployee@gmail.com');
+    expect(e.getRole()).toBe(testValue);
+});
