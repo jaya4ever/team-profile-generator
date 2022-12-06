0b0ffa520c458dbe424a66ddb3cdd9ae53c4c57c
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-06 09:38:09 -0600

    added constructor path

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// Adding all the requirements
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const jest = require('jest');
+
+// Working on constructor by adding their path 
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
